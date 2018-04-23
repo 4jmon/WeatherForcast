2673b8e4c181955b1498673836c479796d7905fe
Abhijit Mondal <abhijit.mondal@ise-indy.com>
2018-04-23 15:45:31 +0530

    test correction

--- src/app/service/data.service.spec.ts
+++ src/app/service/data.service.spec.ts
@@ -1,13 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { Http } from '@angular/http';
 import { DataService } from './data.service';
 
 describe('DataService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-    	imports :[HttpClientModule],
+
     	
-      providers: [DataService]
+      providers: [{
+		  provide: Http},DataService]
     });
   });
 
